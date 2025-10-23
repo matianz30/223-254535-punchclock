e973708bbaa9af3e69c20d96ac0ab824f27b6856
Matian Dauti <matian.dauti@adnovum.ch>
2025-10-23 15:54:44 +0200

    add delete and update

---
+++ src/main/java/ch/zli/m223/controller/EntryController.java
@@ -0,0 +1,56 @@
+package ch.zli.m223.controller;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import ch.zli.m223.model.Entry;
+import ch.zli.m223.service.EntryService;
+
+@Path("/entries")
+@Tag(name = "Entries", description = "Handling of entries")
+public class EntryController {
+
+    @Inject
+    EntryService entryService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Index all Entries.", description = "Returns a list of all entries.")
+    public List<Entry> index() {
+        return entryService.findAll();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Creates a new entry.", description = "Creates a new entry and returns the newly added entry.")
+    public Entry create(Entry entry) {
+       return entryService.createEntry(entry);
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    @Operation(summary = "Deletes an entry.", description = "Deletes the entry with the given id.")
+    public Response delete(@PathParam("id") Long id) {
+      entryService.deleteEntry(id);
+      return Response.status(Response.Status.ACCEPTED).build();}
+
+    @PATCH
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    @Operation(summary = "Updates an entry.", description = "Updates the entry with the given id.")
+    public Entry update(@PathParam("id") Long id, Entry updatedEntry) {
+      updatedEntry.setId(id);
+      return entryService.updateEntry(updatedEntry);
+    }
+}
